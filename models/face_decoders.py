124b4686a89fb85b3e289482e9f5af5e752fc0fc
Bai Yeqi <981011byq@gmail.com>
2023-02-18 22:12:26 +0800

    Remove redundant imports

--- models/face_decoders.py
+++ models/face_decoders.py
@@ -22,10 +22,6 @@ try:
     from .networks import upBlock, GLU
 except:
     from networks import upBlock, GLU
-# try:
-#     from .pgan import PGan_Net
-# except:
-#     from pgan import PGan_Net
 
 
 class DeConv2DBLK(nn.Module):
